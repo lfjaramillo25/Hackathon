1c751cbae943ca399a072ea9738736ec176c8c3f
Leonardo Jaramillo <l.jaramillo1019@gmail.com>
2021-06-23 14:22:00 -0500

    add the bar menu functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-
-/*Like by Heart */
-var heart = document.getElementsByClassName('heart-like');
-var likeHeart = [];
+/*Like by Heart function*/
+let heart = document.getElementsByClassName('heart-like');
+let likeHeart = [];
 function likeHeard(n) {
   if(likeHeart[n] == false || likeHeart[n] == undefined) {
     heart[n].className = "fas fa-heart red-head heart-like";
@@ -15,4 +14,46 @@ function likeHeard(n) {
   }
 }
 
+/*Menu Fuction*/
+let menuShow = document.getElementById("menu-shop");
+let stateMenu = false;
+/*Show the meny */
+function menu() {
+  if(stateMenu == false){
+    console.log("Abracadabra! xD");
+    menuShow.style.display = "block";
+    stateMenu = true;
+  } else {
+    console.log("Me voy D:");
+    menuShow.style.display = "none";
+    stateMenu = false;
+  }
+}
+/*select the options */
+let options = document.getElementsByClassName("options");
+let selectOption = document.getElementById('select-option');
+console.log(options);
+function SlOption(n) {
+  switch(n){
+    case n = 0:
+      console.log("");
+      selectOption.innerText = "Mejor puntaje";
+    break;
+    case n = 1:
+      console.log("caso dos");
+      selectOption.innerText = "Lo más comprado";
+    break;
+    case n = 2:
+      console.log("caso 3");
+      selectOption.innerText = "Tus favoritos";
+    break;
+  }
+  options[n].className = "current options";
+  for(let i = 0; i < options.length; i++){
+    if( i !== n){
+      options[i].className = "options";
+    }
+  }
+  
+}
 
